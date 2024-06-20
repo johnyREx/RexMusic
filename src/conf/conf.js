fe7196ba1f30be5e40b9c6dcb69f3b246ea5f4c9
johnyREx <johnkwofie99@icloud.com>
2024-06-20 12:33:17 +0200

    all good still more work to do

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,2 +1,4 @@
-export const rapidApikey = String(import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY);
+// src/conf/conf.js
+
+export const rapidApiKey = String(import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY);
 export const geoApiKey = String(import.meta.env.VITE_GEO_API_KEY);
